2225a1239908c2850a0b994bc4e95a833c799ae6
Alex Bertolla <alex.bertolla@gmail.com>
2020-01-15 17:40:06 -0300

    hog

---
+++ escala_cinza.py
@@ -0,0 +1,4 @@
+
+def rgbTogray(red, green, blue):
+    cinza = (0.2125 * red) + (0.7154 * green) + (0.0721 * blue)
+    return cinza
--- hog_descriptor.py
+++ hog_descriptor.py
@@ -1,7 +1,7 @@
 from matplotlib import pylab as pylab
-from skimage.io import imread
+from skimage.io import imread, imsave, imshow
 from skimage import exposure
-from skimage.color import rgb2gray, rgb2hsv, rgb2ycbcr, hsv2rgb, ycbcr2rgb, gray2rgb
+from skimage.color import rgb2gray, rgb2hsv, rgb2ycbcr, hsv2rgb, ycbcr2rgb, gray2rgb, rgb2xyz
 from skimage.feature import corner_harris, corner_subpix, corner_peaks, shape_index, hog
 from skimage.transform import warp, SimilarityTransform, AffineTransform, resize
 import cv2
@@ -12,93 +12,97 @@ from skimage.exposure import rescale_intensity
 from skimage.measure import ransac
 from PIL import Image
 
-image = imread('imagens/imagem_77_seg.jpg')
-image2 = imread('imagens/imagem_88.jpg')
+from escala_cinza import rgbTogray
 
-image_gray = rgb2gray(imread('imagens/imagem_77_seg.jpg'))
-fd_gray, image_hog_gray = hog(image_gray, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+image_8_original = imread('imagens/imagem_51.jpg')
+image_8 = imread('imagens/imagem_8_seg.jpg')
+image_67 = imread('imagens/imagem_67_seg.jpg')
+image_77 = imread('imagens/imagem_77_seg.jpg')
+image_88 = imread('imagens/imagem_88_seg.jpg')
 
-
-fd, image_hog = hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-fd2, image2_hog2 = hog(image2, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-
-#print(image.shape)
-linha, coluna, dimension = image.shape
-
-image_red = np.zeros([256, 256])
-image_green = np.zeros([256, 256])
-image_blue = np.zeros([256, 256])
-nova_imagem = np.zeros([256, 256])
-for l in range(linha):
-    for c in range(coluna):
-        image_red[l, c] = int(image[l, c, 0])
-        image_green[l, c] = int(image[l, c, 1])
-        image_blue[l, c] = int(image[l, c, 2])
-        r = image_red[l, c]
-        pixel_c = int((0.2125 * int(r)) + (0.7154 * image_green[l, c]) + (0.0721 * image_blue[l, c]))
-        nova_imagem[l, c] = pixel_c
-        if pixel_c < 255:
-
-            if image_hog[l, c] > 0:
-                print('nova_imagem = ', nova_imagem[l, c])
-                print('image_hog = ', image_hog[l, c])
-                print('image_hog_gray = ', image_hog_gray[l, c])
-                print('image_gray = ', image_gray[l, c])
+image_77_gray = rgb2gray(image_77)
 
 
+fd, image_8_hog = hog(image_8, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+fd, image_8_original_hog = hog(image_8_original, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
 
-#print(image_red)
-#pixel_max = 0
-#pixel_min = 0
-#nova_imagem = np.zeros([256, 256])
+fd, image_67_hog = hog(image_67, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+fd, image_77_hog = hog(image_77, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+fd, image_88_hog = hog(image_88, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
 
 
-#limiar >=0.898 <= 0.996
+#fig, (axes11, axes12, axes13, axes14) = pylab.subplots(1, 4, figsize=(15, 10), sharex=True, sharey=True)
+#fig, (axes21, axes22, axes23, axes24) = pylab.subplots(1, 4, figsize=(15, 10), sharex=True, sharey=True)
+
+fig, plot = pylab.subplots(3, 6)
+
+image_8_hog_rescaled = exposure.rescale_intensity(image_8_hog, in_range=(0, 10))
+image_8_original_hog_rescaled = exposure.rescale_intensity(image_8_original_hog, in_range=(0, 10))
+
+image_67_hog_rescaled = exposure.rescale_intensity(image_67_hog, in_range=(0, 10))
+image_77_hog_rescaled = exposure.rescale_intensity(image_77_hog, in_range=(0, 10))
+image_88_hog_rescaled = exposure.rescale_intensity(image_88_hog, in_range=(0, 10))
+
+plot[0, 0].set_title('Image 8')
+plot[0, 0].imshow(image_8)
+plot[1, 0].imshow(image_8_hog, cmap='gray')
+
+plot[0, 1].set_title('Image 67')
+plot[0, 1].imshow(image_67)
+plot[1, 1].imshow(image_67_hog, cmap='gray')
+
+plot[0, 2].set_title('Image 77')
+plot[0, 2].imshow(image_77)
+plot[1, 2].imshow(image_77_hog, cmap='gray')
+
+plot[0, 3].set_title('Image 88')
+plot[0, 3].imshow(image_88)
+plot[1, 3].imshow(image_88_hog, cmap='gray')
+
+image_conv = image_8 * image_67 * image_77 * image_88
+fd, image_conv_hog = hog(image_conv, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+plot[0, 4].set_title('Convolution')
+plot[0, 4].imshow(image_conv, cmap='gray')
+plot[1, 4].imshow(image_conv_hog, cmap='gray')
+
+image_8_hog_conv = image_8_hog * image_conv_hog
+plot[2, 0].imshow(image_8_hog_conv, cmap='gray')
+
+image_67_hog_conv = image_67_hog * image_conv_hog
+plot[2, 1].imshow(image_67_hog_conv, cmap='gray')
+
+image_77_hog_conv = image_77_hog * image_conv_hog
+plot[2, 2].imshow(image_77_hog_conv, cmap='gray')
+
+image_88_hog_conv = image_88_hog * image_conv_hog
+plot[2, 3].imshow(image_88_hog_conv, cmap='gray')
+
+plot[0, 5].set_title('Image 8 Original')
+plot[0, 5].imshow(image_8_original)
+plot[1, 5].imshow(image_8_original_hog, cmap='gray')
+
+image_8_original_hog_conv = image_8_original_hog * image_conv_hog
+plot[2, 5].imshow(image_8_original_hog_conv, cmap='gray')
+
+#pylab.show()
+
+
+nova_imagem = np.array([np.zeros([256, 256])], [np.zeros([256, 256])])
+
+print(nova_imagem.shape)
+#print(nova_imagem[0, 0])
+#print(image_8_original[0, 0])
+linha, coluna = image_8_original_hog_conv.shape
+
 #for l in range(linha):
 #    for c in range(coluna):
-#        print(image[l, c])
-#        nova_imagem[l, c] = 1.0
-#        print(hog_image[l, c])
-#        if image[l, c] >= 0.898 and image[l, c] <= 0.997:
-#            print(image[l, c])
-#            nova_imagem[l, c] = image[l, c]
-#        else:
-#            image2[l, c] = 128
+#        if image_8_original_hog_conv[l, c] > 0:
+            #nova_imagem[l, c] = image_8_original[l, c, 0]
 
-fig, (axes1, axes2, axes3) = pylab.subplots(1, 3, figsize=(15, 10), sharex=True, sharey=True)
-image_hog_rescaled = exposure.rescale_intensity(image_hog, in_range=(0, 10))
-image_hog_rescaled2 = exposure.rescale_intensity(image2_hog2, in_range=(0, 10))
+            #print(image_8_original[l, c])
 
-axes1.axis('off'), axes1.imshow(image), axes1.set_title('Imput Image')
-axes2.axis('off'), axes2.imshow(image2), axes2.set_title('Histogram of Oriented Gradients')
-
-img_conv = exposure.rescale_intensity(image2_hog2 * image_hog, in_range=(0, 10))
-axes3.axis('off'), axes3.imshow(nova_imagem, cmap='gray'), axes3.set_title('TESTE')
-
-
-#pylab.figure(figsize=(10, 5))
-#pylab.axis('off')
+#pylab.imshow(image_8_original)
 #pylab.imshow(nova_imagem, cmap='gray')
-
-#print(gray2rgb(nova_imagem))
-
-#pylab.hist(image_red.ravel())
-#axes1.imshow(image_red, cmap='gray')
-#pylab.plot()
 #pylab.show()
-
-#pylab.hist(image_green.ravel())
-#axes2.imshow(image_green, cmap='gray')
-#pylab.plot()
-#pylab.show()
-
-#pylab.hist(image_blue.ravel())
-#axes3.imshow(image_blue, cmap='gray')
-#pylab.plot()
-
-
-
-#pylab.show()
-
 
 print('FIM')
