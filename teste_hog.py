2e34ac5ca8f502d3f80ee8ddd21ec5c6f86daa41
Alex Bertolla <alex.bertolla@gmail.com>
2020-01-29 14:54:19 -0300

    inclusão de imagens não lagartas

--- teste_hog.py
+++ teste_hog.py
@@ -18,28 +18,40 @@ import glob
 train_path_lagarta = 'imagens/treinamento/lagarta'
 train_names_lagarta = os.listdir(train_path_lagarta)
 feature_vector = []
+hog_descriptor = cv2.HOGDescriptor()
 
 for imagem in train_names_lagarta:
     for file in glob.glob(train_path_lagarta + '/' + imagem):
         image_rgb = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB)
         fd, image_hog = hog(image_rgb, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
         linha, coluna = image_hog.shape
-        lista = []
         for l in range(linha):
             for c in range(coluna):
                 if image_hog[l, c] > 0:
-                    lista.append([np.array(image_hog[l, c])])
+                    feature_vector.append([np.array(image_hog[l, c])])
+                    hog_descriptor.setSVMDetector(feature_vector)
                     #feature_vector.append(image_hog[l, c])
                     #feature_vector.append(lista)
 
 
-#hog_descriptor = cv2.HOGDescriptor()
-#print(hog_descriptor)
-hog_people = cv2.HOGDescriptor_getDefaultPeopleDetector()
-print(hog_people)
-#array_lista = np.asanyarray(lista)
-lista = np.asarray(lista)
-print(lista)
+
+
+feature_vector = np.asarray(feature_vector)
+
+imagem_analise = cv2.cvtColor(cv2.imread('imagens/teste/imagem_1.jpg'), cv2.COLOR_BGR2RGB)
+
+#hog_compute = hog_descriptor.compute(imagem_analise)
+#print(cv2.HOGDescriptor_get)
+
+#(foundBoundingBoxes, weights) = hog_descriptor.detectMultiScale(imagem_analise, winStride=(4, 4), padding=(8, 8), scale=1.02, finalThreshold=0)
+#print(foundBoundingBoxes)
+
+
+
+#pylab.imshow(hog_compute)
+#pylab.show()
+
+
 
 
 
