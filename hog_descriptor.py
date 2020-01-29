36e2a0377858705e920126b0b90e6fe3703c1fcf
Alex Bertolla <alex.bertolla@gmail.com>
2020-01-29 16:54:37 -0300

    commit

--- hog_descriptor.py
+++ hog_descriptor.py
@@ -15,7 +15,14 @@ from PIL import Image
 import os
 import glob
 from sklearn.svm import LinearSVC
+import mahotas as mt
 
+
+def extract_features(image, image_label):
+    textures = mt.features.haralick(image)
+    #read_textures(textures, image_label)
+    ht_mean = textures.mean(axis=0)
+    return ht_mean
 
 def ler_imagens_treinamento(diretorio, lista_imagens, classe):
     array_hog = []
@@ -24,18 +31,57 @@ def ler_imagens_treinamento(diretorio, lista_imagens, classe):
         for file in glob.glob(diretorio + '/'+imagem):
 #            print('Processing Image {}'. format(imagem))
             image_rgb = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB)
-#            gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
-            fd, image_hog = hog(image_rgb, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+
+            gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
+            features = extract_features(gray, imagem)
+            feature_vector_haralick.append(features)
+            label_haralick.append(classe)
+
+            fd, image_hog = hog(image_rgb, orientations=3, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
+            cv2.imshow(imagem, image_hog)
+            cv2.waitKey(0)
+
             linha, coluna = image_hog.shape
+
             for l in range(linha):
                 for c in range(coluna):
                     if image_hog[l, c] > 0:
                         train_features.append(image_hog[l, c])
                         train_labels.append(classe)
 
+def resultado_svm(svm_class):
+    print('[STATUS] Starting tests..')
+    test_path = "imagens/teste"
+    test_names = os.listdir(test_path)
+    for test_name in test_names:
+        cur_path = test_path + '/' + test_name
+        cur_label = test_name
+        i = 1
+        for file in glob.glob(cur_path):
+            # read the input image
+            image = cv2.imread(file)
+            cv2.imshow(cur_label, image)
+
+            # convert to grayscale
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # extract haralick texture from the image
+            features = extract_features(gray, cur_label)
+
+            # evaluate the model and predict label
+            prediction = svm_class.predict(features.reshape(1, -1))[0]
+
+            # show the label
+            cv2.putText(image, prediction, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), 3)
+            print('Prediction - {}'.format(prediction))
+
+            # display the output image
+            cv2.imshow(cur_label, image)
+            cv2.waitKey(0)
 
 classe_spodoptera = 'Spodoptera'
 classe_outra_lagarta = 'Outra lagarta'
+classe_nao_lagarta = 'Nao lagarta'
 
 train_path_lagarta = 'imagens/treinamento/lagarta'
 train_names_lagarta = os.listdir(train_path_lagarta)
@@ -43,8 +89,14 @@ train_names_lagarta = os.listdir(train_path_lagarta)
 train_path_outra_lagarta = 'imagens/treinamento/outra_lagarta'
 train_names_outra_lagarta = os.listdir(train_path_outra_lagarta)
 
+train_path_nao_lagarta = 'imagens/treinamento/nao_lagarta'
+train_names_nao_lagarta = os.listdir(train_path_nao_lagarta)
+
 train_features = []
 train_labels = []
+
+feature_vector_haralick = []
+label_haralick = []
 
 array_caracteristicas = []
 
@@ -55,65 +107,17 @@ x = 0
 y = 0
 #LER IMAGENS TREINAMENTO SPODOPTERA FUNGIPERD
 
-print(train_features)
-print(train_labels)
+
+ler_imagens_treinamento(train_path_lagarta, train_names_lagarta, classe_spodoptera)
+#ler_imagens_treinamento(train_path_nao_lagarta, train_names_nao_lagarta, classe_nao_lagarta)
 
 
-ler_imagens_treinamento(train_path_outra_lagarta, train_names_outra_lagarta, classe_outra_lagarta)
-#ler_imagens_treinamento(train_path_lagarta, train_names_lagarta, classe_spodoptera)
+print('[STATUS] Creating the classifier..')
+clf_svm = LinearSVC(random_state=0 )
 
+print('[STATUS] Fitting data/label to model..')
+clf_svm.fit(feature_vector_haralick, label_haralick)
 
-print(train_labels)
-print(train_features)
-
-
-#array_caracteristicas_outras_lagartas = ler_imagens_treinamento(train_path_outra_lagarta, train_names_outra_lagarta, classe_outra_lagarta)
-#for train_name_lagarta in train_names_lagarta:
-#    cur_path = train_path_lagarta + '/' + train_name_lagarta
-#    cur_label = train_name_lagarta
-
-#    for file in glob.glob(cur_path):
-#        print('Processing Image - {} in {}'. format(i, cur_label))
-#        image = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB)
-#        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#        fd, image_hog = hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-#        array_hog = []
-#        linha, coluna = image_hog.shape
-#        for l in range(linha):
-#            for c in range(coluna):
-#                if image_hog[l, c] > 0:
-#                    array_hog.append([cur_label, l, c, image_hog[l, c]])
-#       array_imagem_hog.append(array_hog)
-
-#print(array_imagem_hog)
-#        pylab.figure(figsize=(4, 4))
-#        pylab.imshow(image_hog)
-        #train_features.append(features)
-        #train_labels.append(classe_spodoptera)
-
-#pylab.show()
-
-#fd, image_8_hog = hog(image_8, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-#fd, image_8_original_hog = hog(image_8_original, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-
-#fd, image_67_hog = hog(image_67, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-#fd, image_77_hog = hog(image_77, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-#fd, image_88_hog = hog(image_88, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True)
-
-
-#fig, (axes11, axes12, axes13, axes14) = pylab.subplots(1, 4, figsize=(15, 10), sharex=True, sharey=True)
-#fig, (axes21, axes22, axes23, axes24) = pylab.subplots(1, 4, figsize=(15, 10), sharex=True, sharey=True)
-
-#fig, plot = pylab.subplots(3, 6)
-
-#image_8_hog_rescaled = exposure.rescale_intensity(image_8_hog, in_range=(0, 10))
-#image_8_original_hog_rescaled = exposure.rescale_intensity(image_8_original_hog, in_range=(0, 10))
-
-#image_67_hog_rescaled = exposure.rescale_intensity(image_67_hog, in_range=(0, 10))
-#image_77_hog_rescaled = exposure.rescale_intensity(image_77_hog, in_range=(0, 10))
-#image_88_hog_rescaled = exposure.rescale_intensity(image_88_hog, in_range=(0, 10))
-
-
-
+#resultado_svm(clf_svm)
 
 print('FIM')
